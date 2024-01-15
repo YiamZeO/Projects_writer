111e0a9818df6132fe95b546598a2794eb8adde3
The0 <yiamzeo@gmail.com>
2024-01-16 00:24:32 +0300

    1

--- src/main/java/ru/main/projects_writer/СompanyWriterApplication.java
+++ src/main/java/ru/main/projects_writer/СompanyWriterApplication.java
@@ -19,7 +19,7 @@ public class СompanyWriterApplication extends Application {
         companyWriterController = fxmlLoader.getController();
         companyWriterController.setCurrentStage(stage);
         Scene scene = new Scene(root, 799, 609);
-        stage.setTitle("Компании");
+        stage.setTitle("Компанииu");
         stage.setScene(scene);
         stage.getIcons().add(new Image("file:icon.png"));
         stage.show();
